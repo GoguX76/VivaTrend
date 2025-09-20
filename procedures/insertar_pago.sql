bb2ab399fdb7c0dc13f0bfff383aac5052be94df
GoguX76 <se.godoyr@duocuc.cl>
2025-09-20 13:10:20 -0300

    Creación de nueva secuencia para la creación de tarjetas. Modificación de registrar_pago para que inserte correctamente las tarjetas y maneje excepciones. Modificación en registrar_pago para manejar mejor los datos e inserciones de manera aleatoria. Falta cambiar la longitud de la columna de nro_secuencia_pago para que acepte más valores.

--- procedures/insertar_pago.sql
+++ procedures/insertar_pago.sql
@@ -1,9 +1,26 @@
+DECLARE
+    v_numrun NUMBER;
+    v_nro_tarjeta NUMBER;
+    v_cod_forma_pago NUMBER;
+    v_monto NUMBER;
 BEGIN
+    
+    SELECT NUMRUN INTO v_numrun FROM CLIENTE
+    WHERE ROWNUM = 1;
+
+    v_cod_forma_pago := NULL;
+    v_monto := TRUNC(DBMS_RANDOM.VALUE(10000, 100001));
+
     registrar_pago(
-        12345678,
-        987654321,
-        2,
-        50000,
-        TO_DATE('18-09-2025', 'DD-MM-YYYY')
+        v_numrun,
+        v_nro_tarjeta,
+        v_cod_forma_pago,
+        v_monto,
+        SYSDATE
     );
+
+    DBMS_OUTPUT.PUT_LINE('NRO_TARJETA generado: ' || v_nro_tarjeta);
+
 END;
+/
+COMMIT;
